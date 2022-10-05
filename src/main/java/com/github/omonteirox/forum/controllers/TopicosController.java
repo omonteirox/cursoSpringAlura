f2c631fcfbd05e9cbe50ca7b5eb1e0f520ab8dbb
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-10-05 02:27:04 -0300

    Utilizando o CacheEvict quando realizar atualização e exclusão de Topicos

--- src/main/java/com/github/omonteirox/forum/controllers/TopicosController.java
+++ src/main/java/com/github/omonteirox/forum/controllers/TopicosController.java
@@ -8,6 +8,7 @@ import com.github.omonteirox.forum.models.Topico;
 import com.github.omonteirox.forum.repository.CursoRepository;
 import com.github.omonteirox.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -44,6 +45,7 @@ public class TopicosController {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder) {
         Topico topico = topicoForm.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -61,6 +63,7 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> optional = topicoRepository.findById(id);
         if (optional.isPresent()) {
@@ -72,6 +75,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id) {
         Optional<Topico> topico = topicoRepository.findById(id);
         if (topico.isPresent()) {
